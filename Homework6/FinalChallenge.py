0f7e14dabc00cd15e13afd867cc740538fcc311f
prajwalfc <prajwal_k@live.com>
2019-08-04 10:48:32 -0400

    formatted

--- Homework6/FinalChallenge.py
+++ Homework6/FinalChallenge.py
@@ -1,14 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[7]:
 
 
 from pyspark import SparkContext
-
-
-# In[8]:
-
 
 def createIndex(shapefile):
     import rtree
@@ -27,70 +22,56 @@ def findZone(p, index, zones):
             return idx
 
 
-# In[14]:
 
-
-def getDrugsFromFileToSet(fn1,fn2):
+def getDrugsFromFileToSets(fn1,fn2):
+    import re
     w=set()
-    phrase = set()
     with open(fn1) as reader:
         rows = reader.read()
         for row in rows.split("\n"):
-            if " " in row:
-                phrase.add(row.strip())
-            else:
-                w.add(row.strip())
+            w.add(" ".join(re.split(r'\W+',row)))
     with open(fn2) as reader:
         rows = reader.read()
         for row in rows.split("\n"):
-            if " " in row:
-                phrase.add(row.strip())
-            else:
-                w.add(row.strip())
-    return w,phrase
-# def lineToBigrams(tweets):
-#     import nltk
-#     tweet_bigram = ()
-#     tweet_token = ()
-#     for tweet in tweets[0][3:]:
-#         tweet_token+=tuple(tweet.split(" "))
-#         tweet_bigram += tuple(nltk.bigrams(nltk.word_tokenize(tweet)))
-#     tweet_bigram_refine = tuple(map(lambda x: " ".join(x).lower(),tweet_bigram))
-#     tweet_token_refine = tuple(map(lambda x: x.lower(),tweet_token))
-#     return (tweet_token_refine+tweet_bigram_refine)
-# def testToBigram(tweets):
-#     drug_tweet = 0
-#     tweet_token = lineToBigrams(tweets)
-#     drug_name_set = getDrugsFromFileToSet('drug_illegal.txt','drug_sched2.txt')
-#     if(drug_name_set.intersection(set(tweet_token))):
-#         drug_tweet = 1
-#     return tweets[0][1:3],drug_tweet
-# def filterDrugs(tweets):
-#     import re
-#     a,b = getDrugsFromFileToSet('drug_illegal.txt','drug_sched2.txt')
-#     for tweet in tweets[2]:
-#         for drug in b:
-#             sets = re.findall(r' '+drug+' |^'+drug+' | '+drug+'$',tweet)
-#             if sets:
-#                 return True
-#     return False
-def filterDrugs(tweets):
+            w.add(" ".join(re.split(r'\W+',row)))
+    return w
+
+'''function filterTweetWithoutDrugsKeyword takes tweet and drugs_set.
+drug_set consists of drug related keywords.
+if we have a tweet like "I study at CCNY" then we split the tweet to store in list as ["I","study","at","CCNY"]
+then we check the contents of tweet if we have it in drug_set or not.
+The check is done as in the sequences of string:
+'I'
+'I study'
+'I study at'
+'I study at CCNY'
+'study'
+'study at'
+'study at CCNY'
+'at'
+'at CCNY'
+'CCNY'
+I any of the above string is contained in the drug_set, the function returns True skipping the rest check.
+# In[3]:
+'''
+
+def filterTweetWithoutDrugsKeyword(tweet,drugs_set):
     import re
-    a,b = getDrugsFromFileToSet('drug_illegal.txt','drug_sched2.txt')
-    for tweet in tweets[2]:
-        sets = a.intersection(set(tweet.split(" ")))
-        if sets:
-            print("word----------------",sets)
+    lst = re.split(r'\W+',tweet)
+    lst = list(filter(lambda x:x!="",lst))
+    i=0
+    for word1 in lst:
+        j=i+1
+        temp =word1
+        if(temp in drugs_set):
             return True
-        else:
-            for drug in b:
-                if drug in tweet:
-                    print("phrase--------------",drug)
-                    return True
+        for word2 in lst[j:]:
+            temp = temp+" "+word2
+            if(temp in drugs_set):
+                return True
+        i=i+1
     return False
 
-
-# In[15]:
 
 
 def partition(tweetData):
@@ -98,6 +79,7 @@ def partition(tweetData):
     import shapely.geometry as geom
     proj = pyproj.Proj(init="epsg:4326", preserve_units=False)
     main_index , main_zone = createIndex('500cities_tracts.geojson')
+    drugs_set = getDrugsFromFileToSets('drug_illegal.txt','drug_sched2.txt')
     city_index = None
     for tweet in tweetData:
         tweet_split = tweet.split('|')
@@ -112,28 +94,20 @@ def partition(tweetData):
             pass
             #print(tweet_split[0])
         else:
-            yield(main_zone['plctract10'][city_index],main_zone['plctrpop10'][city_index],tweet_split[5:])
+            if (int(main_zone['plctrpop10'][city_index])!=0) and filterTweetWithoutDrugsKeyword(tweet_split[5],drugs_set):
+                yield(main_zone['plctract10'][city_index],main_zone['plctrpop10'][city_index]),1.0
 
 
-# In[11]:
 
 
-def mapper(data):
-    return (data[0],data[1]),1
 def normalizer(data):
-    return data[0][0],data[1]/data[0][1]
+    return data[0][0],float(data[1])/float(data[0][1])
 
-
-# In[16]:
 
 
 if __name__=='__main__':
-    #sc = SparkContext()
-    rdd = sc.textFile('input.txt')
-    rdd.mapPartitions(partition)        .filter(filterDrugs)        .map(mapper)        .reduceByKey(lambda x,y:x+y)        .map(normalizer)        .saveAsTextFile("res_opt19")
-
-
-# In[ ]:
+    sc = SparkContext()
+    sc.textFile('/data/share/bdm/tweets-100m.csv').mapPartitions(partition).reduceByKey(lambda x,y:x+y).map(normalizer).sortBy(lambda x:x[0]).saveAsTextFile("final007")
 
 
 
